6be330c574a28f851ecc09934ab92b540e77be7f
sk698 <sk9006038@gmail.com>
2025-11-10 02:16:06 +0530

    Refactor API base URL configuration to use a single environment variable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+        proxy: {
+          '/api': { // The path that will be proxied
+            target: 'https://python-production-2bac.up.railway.app', // The address of your backend server
+            changeOrigin: true, // Changes the origin of the request to match the target URL
+            // rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrites the path
+            // secure: false, // Optional: disables SSL verification for local development
+            // ws: true, // Optional: proxy websockets
+          },
+        },
+      },
 })
--- my-app/src/api.ts
+++ my-app/src/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-// Get variables from .env
-const API_IP = process.env.EXPO_PUBLIC_API_IP || '192.168.1.6'; // Fallback
-const API_PORT = process.env.EXPO_PUBLIC_API_PORT || '8000';   // Fallback
 
-const API_BASE_URL = `http://${API_IP}:${API_PORT}`;
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
