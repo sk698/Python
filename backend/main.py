cde6430d1fc5e045abc144156e43e0d15460a819
sk698 <85735810+sk698@users.noreply.github.com>
2025-11-10 00:41:19 +0530

    PORT CONFIGURE

--- backend/main.py
+++ backend/main.py
@@ -88,7 +88,8 @@ try:
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")
     )
     cursor = conn.cursor(dictionary=True)
     print("Database connection successful!")
